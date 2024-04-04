1456aa5a18ad4fe135dab621a09e364bbe790bb5
root <andygyakobo@gmail.com>
2024-04-04 01:31:13 -0400

    Trying to implement the Bigram Language Model

---
+++ bigramlm.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+torch.manual_seed(1337)
+
+class BigramLanguageModel(nn.Module):
+    def __init__(self, vocab_size):
+        super().__init__()
+        # Each token reads off the logits for next token from a lookup table 
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets=None):
+        # idx and targets are both (B,T) tensor of integers
+        logits = self.token_embedding_table(idx) # (B, T, C)
+        
+        B, T, C = logits.shape
+        logits = logits.view(B*T, C)
+        targets = targets.view(B*T)
+        loss = F.cross_entropy(logits, targets)
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        for _ in range(max_new_tokens):
+            # get the predictions
+            logits, loss = self(idx)
+
+            # focus only on the last time step
+            logits = logits[:, -1, :]
+
+            # apply softmax to get probabilites
+            probs = F.softmax(logits, dim=-1)
+
+            # sample from the distribution
+            idx_next = torch.mulinomial(probs, num_sample=1)
+
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1)
+        return idx
+
+    
+
+
+
--- main.py
+++ main.py
@@ -14,7 +14,6 @@ itos = { i:ch for i, ch in enumerate(chars) }
 encode = lambda s: [stoi[c] for c in s] # encoder: takes a string, output a list of integers
 decode = lambda l: ''.join([itos[i] for i in l]) # decoder: takes a list of integers, output a string
 
-# print(encode("a"))
 # print(encode("hii there"))
 # print(decode(encode("hii there")))
 
@@ -22,7 +21,7 @@ decode = lambda l: ''.join([itos[i] for i in l]) # decoder: takes a list of inte
 import torch
 data = torch.tensor(encode(text), dtype=torch.long)
 print(data.shape, data.dtype)
-print(data[:1000]) # the 1000 characters we looked at earlier will look like this to the GPT 
+# print(data[:1000]) # the 1000 characters we looked at earlier will look like this to the GPT 
 
 # Let's now split up the data into train and validation sets
 # This will help us understand to what extent our model is overfitting
@@ -30,3 +29,42 @@ n = int(0.9 * len(data)) # the first 90% will be train, rest val
 train_data = data[:n]
 val_data = data[n:]
 
+'''
+#tain_data[:block_size+1] # => ([ 18, 47, 56, 57, 1, 15, 47, 58 ])
+
+# Shows a truncating example
+block_size = 8
+x = train_data[:block_size] # Input to the transformer
+y = train_data[1:block_size+1] # The next block size characters
+for t in range(block_size):
+    content = x[:t+1]
+    target = y[t]
+    print(f"when input is {content} the target: {target}")
+'''
+
+torch.manual_seed(1337) # Random number coefficient
+batch_size = 4 # How many independent sequences will we process in parallel?
+block_size = 8 # Maximum context length for predictions
+
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    return x, y
+
+xb, yb = get_batch('train')
+print('inputs:')
+print(xb.shape)
+print(xb)
+print('target:') 
+print(yb.shape)
+print(yb)
+print('-----')
+
+from bigramlm import *
+m = BigramLanguageModel(vocab_size)
+logits, loss = m(xb, yb)
+print(logits.shape)
+print(loss)
