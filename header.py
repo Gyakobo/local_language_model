e7fe50be4bedb4e430d0195cb0d4f46a38de4d6e
Gyakobo <andygyakobo@gmail.com>
2024-04-13 07:32:56 -0400

    Trying to fix merge error

--- header.py
+++ header.py
@@ -2,11 +2,11 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-
 # Hyperparameters
 batch_size = 64 # how many independent sequences will we process in parallel?
 block_size = 256 # what is the maximum context length for predictions?
 max_iters = 5000 
+
 eval_interval = 500
 learning_rate = 3e-4
 # eval_iters = 200
