ee2bc46471e4d0e5fccaedd8978497920d86369f
root <andygyakobo@gmail.com>
2024-04-30 01:52:22 -0400

    Transformer effectively working

--- speech.py
+++ speech.py
@@ -1,24 +1,28 @@
 import torch
-from transformers import GPT2Model, GPT2Tokenizer
+from transformers import GPT2Model, GPT2Tokenizer, GPT2LMHeadModel
+from main import *
 
 # Path to your pretrained model
-model_path = "./models/shakespeare_model.pth"
+# model_path = "./models/shakespeare_model.pth"
 # Load the model state_dict
-model_state_dict = torch.load(model_path)
+# model_state_dict = torch.load(model_path)
 # Create an instance of GPT2Model
-model = GPT2Model.from_pretrained(pretrained_model_name_or_path=None, state_dict=model_state_dict)
+# model = GPT2Model.from_pretrained(pretrained_model_name_or_path=None, state_dict=model_state_dict)
 
+model = GPT2LMHeadModel.from_pretrained("gpt2")
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+
+flag = True
 # My input
-input_text = "Your input text goes here."
-input_ids = tokenizer.encode(input_text, return_tensors="pt")
+while flag:
+    input_text = input("input: ") 
+    if input_text == "exit":
+        flag = False
+        continue
 
-'''
-with torch.no_grad():
-    outputs = model(input_ids)
-    embeddings = outputs.last_hidden_state
-'''
+    input_ids = tokenizer.encode(input_text, return_tensors="pt")
 
-output = model.generate(input_ids, max_length=50, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
-decoded_output = tokenizer.decode(output[0], skip_special_tokens=True)
-print(decoded_output)
+    output = model.generate(input_ids, max_length=50, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
+    decoded_output = tokenizer.decode(output[0], skip_special_tokens=True).split('.')[0]
+    print(decoded_output, end="\n\n")
 
